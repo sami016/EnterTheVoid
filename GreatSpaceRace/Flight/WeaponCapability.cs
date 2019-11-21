22a9e85107d0a97a5a796a2f18f5d6a8984df31e
sami016 <samholder@live.co.uk>
2019-11-21 23:29:34 +0000

    added basic chase ai

--- GreatSpaceRace/Flight/WeaponCapability.cs
+++ GreatSpaceRace/Flight/WeaponCapability.cs
@@ -1,4 +1,7 @@
-﻿using Forge.Core.Components;
+﻿using Forge.Core;
+using Forge.Core.Components;
+using Forge.Core.Interfaces;
+using Forge.Core.Utilities;
 using GreatSpaceRace.Projectiles;
 using GreatSpaceRace.Ships.Modules;
 using Microsoft.Xna.Framework;
@@ -9,41 +12,58 @@ using System.Text;
 
 namespace GreatSpaceRace.Flight
 {
-    public class WeaponCapability : Component
+    public class WeaponCapability : Component, ITick
     {
+        private CompletionTimer _lightweightCooldown = new CompletionTimer(TimeSpan.FromSeconds(0.5f));
+
         [Inject] FlightShip FlightShip { get; set; }
         [Inject] Transform Transform { get; set; }
 
+        public WeaponCapability()
+        {
+            _lightweightCooldown.Complete();
+        }
+
         public void StandardFire()
         {
-            var guns = FlightShip.Topology.AllSections
-                .Where(x => x.Module is BlasterModule);
-            var shipTransform = Transform;
-            foreach (var gun in guns)
+            if (_lightweightCooldown.Completed)
             {
-                var flightNode = FlightShip.GetNodeForSection(gun.GridLocation);
-                var rotation = (float)((-gun.Rotation - 2) * Math.PI / 3);
-                var rotationQuat = Quaternion.CreateFromYawPitchRoll(rotation, 0, 0);
-                var nodeTransform = flightNode.Entity.Get<Transform>();
-                var location = Vector3.Transform(Vector3.Zero, nodeTransform.WorldTransform * shipTransform.WorldTransform);
-                var shell = Entity.EntityManager.Create();
-                shell.Add(new Transform
+                var guns = FlightShip.Topology.AllSections
+                    .Where(x => x.Module is BlasterModule);
+                var shipTransform = Transform;
+                foreach (var gun in guns)
                 {
-                    Location = location
-                });
-                shell.Add(
-                    new Projectile(
-                        FlightShip.ShipGuid,
-                        Vector3.Transform(Vector3.Transform(Vector3.Forward, rotationQuat), shipTransform.Rotation), 
-                        20f
-                    )
-                );
+                    var flightNode = FlightShip.GetNodeForSection(gun.GridLocation);
+                    var rotation = (float)((-gun.Rotation - 2) * Math.PI / 3);
+                    var rotationQuat = Quaternion.CreateFromYawPitchRoll(rotation, 0, 0);
+                    var nodeTransform = flightNode.Entity.Get<Transform>();
+                    var location = Vector3.Transform(Vector3.Zero, nodeTransform.WorldTransform * shipTransform.WorldTransform);
+                    var shell = Entity.EntityManager.Create();
+                    shell.Add(new Transform
+                    {
+                        Location = location
+                    });
+                    shell.Add(
+                        new Projectile(
+                            FlightShip.ShipGuid,
+                            Vector3.Transform(Vector3.Transform(Vector3.Forward, rotationQuat), shipTransform.Rotation),
+                            20f
+                        )
+                    );
+                }
+
+                _lightweightCooldown.Restart();
             }
         }
 
         public void HeavyFire()
         {
 
+        }
+
+        public void Tick(TickContext context)
+        {
+            _lightweightCooldown.Tick(context.DeltaTime);
         }
     }
 }
